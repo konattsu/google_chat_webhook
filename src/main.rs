3d00728510cf3e3c6ef182fcafb4b524cb8a0b34
oisi-namagomi <kokonattsu1112@gmail.com>
2024-09-15 21:51:46 +0900

    [update] 超基本的なやつ

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Debug)]
+struct PostMess {
+    text: String,
+}
+impl PostMess {
+    fn new(text: String) -> Self {
+        Self { text }
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv::dotenv().ok();
+    let url = env::var("WEBHOOK_URL").expect("WEBHOOK_URL must be set");
+
+    let mess = PostMess::new("Hello webhook from rust!".into());
+
+    let client = reqwest::Client::new();
+    let res = client
+        .post(url)
+        .body(serde_json::to_string(&mess).unwrap())
+        .send()
+        .await?;
+
+    println!("res: {:?}", res);
+    Ok(())
 }

03fdb56c7bb85d87d427fb72d3726a8ffe00e2c9
oisi-namagomi <kokonattsu1112@gmail.com>
2024-09-18 18:50:11 +0900

    [update] 引数String -> &str

--- src/lib.rs
+++ src/lib.rs
@@ -9,10 +9,10 @@ pub use domain::Message;
 pub use infrastructure::ExternalApiStruct;
 
 pub fn post_google_chat_webhook(
-    webhook_url: String,
+    webhook_url: &str,
     message: Message,
 ) -> Result<(), DomainError> {
-    let api_impl = ExternalApiStruct::new(&webhook_url);
+    let api_impl = ExternalApiStruct::new(webhook_url);
     let service = ChatService::new(api_impl);
     service.post_message_actual_call(message)
 }
